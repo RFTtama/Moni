d116d849bd9d639d6447242f501951f2beabaf6c
tama_RFT <kitgareth4@gmail.com>
2024-10-24 21:53:52 +0900

    不要と思われる機能(それ以外にも依存が深すぎるもの)を削除

---
+++ Moni/SaveData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moni
+{
+    internal class SaveData
+    {
+        public static readonly string filePath = @".\tcData\config.cfg";
+
+        //一旦全削除 設定をJSONファイルで読み取れるようにする
+    }
+}
